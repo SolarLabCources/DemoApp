5021aa6bfbd871e2dd24174e77d383f11516a9e8
Administrator <admin@example.com>
2016-12-15 19:09:39 +0300

    added DAL

---
+++ BusinessLogic/BusinessLogic.Implementation/NewsManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BusinessLogic.Abstraction;
+
+namespace BusinessLogic.Implementation
+{
+    public class NewsManager : INewsManager
+    {
+        public void AddNews()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/DataAccessLayer/DemoContext.cs
@@ -0,0 +1,8 @@
+﻿using System.Data.Entity;
+
+namespace DataAccessLayer
+{
+    public class DemoContext : DbContext
+    {
+    }
+}
