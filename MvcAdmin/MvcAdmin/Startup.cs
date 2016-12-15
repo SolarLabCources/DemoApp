1e7a885d520704302925ae867184d832785e763e
Administrator <admin@example.com>
2016-12-15 18:16:42 +0300

    added core mvc

---
+++ MvcAdmin/MvcAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcAdmin.Startup))]
+namespace MvcAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
